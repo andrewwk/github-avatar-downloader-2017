3f455e5fd8b01a2884ae3436b5f1582f0d2aee8d
Andrew Kim <your@email.com>
2017-01-16 18:06:26 -0500

    Added function that checks for repoowner and reponame.

--- download_avatars.js
+++ download_avatars.js
@@ -6,17 +6,29 @@ const GITHUB_TOKEN = process.env.GITHUB_TOKEN;
 const owner        = process.argv[2];
 const name         = process.argv[3]
 
+const checkForArguments = (repoOwner, repoName) => {
+  const err = new Error("No repo name/owner specified!!!");
+  if (!repoOwner || !repoName) {
+    throw err;
+    // throw console.error("No repo name/owner specified!!!");
+  };
+  if (repoOwner == "" || repoName == "") {
+    throw err;
+    // throw console.error("No repo name/owner specified!!!");
+  };
+};
+
 const downloadImageByURL = (url, filePath) => {
   request.get(url)
-     .on("error", (err) => {
-       throw err;
-     })
-     .on("response", (res) => {
-       console.log(`Response Status Code: ${res.statusCode}\n
-         Status Message${res.statusMessage}\n
-         Content Type: ${res.headers["content-type"]}`);
-     })
-     .pipe(fs.createWriteStream(`./avatars/${filePath}.jpg`));
+    .on("error", (err) => {
+      throw err;
+    })
+    .on("response", (res) => {
+      console.log(`Response Status Code: ${res.statusCode}\n
+        Status Message${res.statusMessage}\n
+        Content Type: ${res.headers["content-type"]}`);
+    })
+    .pipe(fs.createWriteStream(`./avatars/${filePath}.jpg`));
 };
 
 const getAvatarURL = (err, result) => {
@@ -24,20 +36,14 @@ const getAvatarURL = (err, result) => {
     console.log("Errors:", err);
   } else {
     result.forEach((data) => {
-      downloadImageByURL(data.avatar_url, data.login)
+      downloadImageByURL(data.avatar_url, data.login);
     })
   }
 };
 
 const getRepoContributors = (repoOwner, repoName, cb) => {
 
-  if (!repoOwner || !repoName) {
-    return console.log("No repo name/owner specified!!!");
-  }
-  if (repoOwner == "" || repoName == "") {
-    return console.log("No repo name/owner specified!!!");
-  }
-
+  checkForArguments(repoOwner, repoName);
   const options = {
     url: `https://${GITHUB_USER}:${GITHUB_TOKEN}@api.github.com/repos/${repoOwner}/${repoName}/contributors`,
     headers: {
@@ -47,8 +53,8 @@ const getRepoContributors = (repoOwner, repoName, cb) => {
 
   request(options, (err, res, body) => {
     let jsonData = JSON.parse(body);
-    getAvatarURL(err, jsonData)
-  });
+    getAvatarURL(err, jsonData);
+  })
 };
 
 getRepoContributors(owner, name, getAvatarURL);
